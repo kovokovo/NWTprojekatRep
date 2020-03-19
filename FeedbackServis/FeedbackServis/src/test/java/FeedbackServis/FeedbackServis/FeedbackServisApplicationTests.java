2266754a10b5fd9c11335886bdf860a211727121
acesko1 <acesko1@etf.unsa.ba>
2020-03-19 16:54:50 +0100

    Add files via upload
    Dodan Feedback servis

---
+++ FeedbackServis/FeedbackServis/src/test/java/FeedbackServis/FeedbackServis/FeedbackServisApplicationTests.java
@@ -0,0 +1,13 @@
+package FeedbackServis.FeedbackServis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
