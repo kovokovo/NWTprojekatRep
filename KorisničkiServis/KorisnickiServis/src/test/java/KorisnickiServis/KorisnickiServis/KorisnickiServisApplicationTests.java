02ff030baaf18196d9ab2403e9d5f16ac1aacd28
acesko1 <acesko1@etf.unsa.ba>
2020-03-19 16:47:55 +0100

    Add files via upload

---
+++ KorisničkiServis/KorisnickiServis/src/test/java/KorisnickiServis/KorisnickiServis/KorisnickiServisApplicationTests.java
@@ -0,0 +1,13 @@
+package KorisnickiServis.KorisnickiServis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorisnickiServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
