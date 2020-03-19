103c6ed7a1052b89d1deebcf754d724e29fb9011
acesko1 <acesko1@etf.unsa.ba>
2020-03-19 16:51:38 +0100

    Add files via upload
    Dodan servis Zaliha

---
+++ ServisZaliha/ServisZaliha/src/main/java/ServisZaliha/ServisZaliha/ServisZalihaApplication.java
@@ -0,0 +1,13 @@
+package ServisZaliha.ServisZaliha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisZalihaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisZalihaApplication.class, args);
+	}
+
+}
---
+++ ServisZaliha/ServisZaliha/src/test/java/ServisZaliha/ServisZaliha/ServisZalihaApplicationTests.java
@@ -0,0 +1,13 @@
+package ServisZaliha.ServisZaliha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisZalihaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
