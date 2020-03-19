3192e7433a9de5c5f664775b1c60698e0f233ae0
acesko1 <acesko1@etf.unsa.ba>
2020-03-19 16:49:05 +0100

    Add files via upload
    Dodan Autorizacijski servis

---
+++ AutorizacijskiServis/AutorizacijskiServis/src/main/java/AutorizacijskiServis/AutorizacijskiServis/AutorizacijskiServisApplication.java
@@ -0,0 +1,13 @@
+package AutorizacijskiServis.AutorizacijskiServis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizacijskiServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorizacijskiServisApplication.class, args);
+	}
+
+}
---
+++ AutorizacijskiServis/AutorizacijskiServis/src/test/java/AutorizacijskiServis/AutorizacijskiServis/AutorizacijskiServisApplicationTests.java
@@ -0,0 +1,13 @@
+package AutorizacijskiServis.AutorizacijskiServis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizacijskiServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
