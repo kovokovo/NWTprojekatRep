7e56e51217457fa550cf95922afd27a58b707d45
acesko1 <acesko1@etf.unsa.ba>
2020-03-26 12:02:28 +0100

    Update StavkaProdajeServiceImpl.java

--- korisnickiServis/src/main/java/in/acesko/springbootcrud/springbootcrudapi/service/StavkaProdajeServiceImpl.java
+++ korisnickiServis/src/main/java/in/acesko/springbootcrud/springbootcrudapi/service/StavkaProdajeServiceImpl.java
@@ -1,5 +1,41 @@
 package in.acesko.springbootcrud.springbootcrudapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import in.acesko.springbootcrud.springbootcrudapi.dao.StavkaProdajeDAO;
+import in.acesko.springbootcrud.springbootcrudapi.model.StavkaProdaje;
+import java.util.List;
+
+@Service
 public class StavkaProdajeServiceImpl implements StavkaProdajeService {
+
+	@Autowired
+	private StavkaProdajeDAO stavkaProdajeDAO;
+	
+	@Transactional
+	@Override
+	public List<StavkaProdaje> get() {
+		return stavkaProdajeDAO.get();
+	}
+
+	@Transactional
+	@Override
+	public StavkaProdaje get(int id) {
+	return stavkaProdajeDAO.get(id);
+	}
+
+	@Transactional
+	@Override
+	public void save(StavkaProdaje stavkaProdaje) {
+		stavkaProdajeDAO.save(stavkaProdaje);		
+	}
+
+	@Transactional
+	@Override
+	public void delete(int id) {
+		stavkaProdajeDAO.delete(id);
+	}
 
 }
