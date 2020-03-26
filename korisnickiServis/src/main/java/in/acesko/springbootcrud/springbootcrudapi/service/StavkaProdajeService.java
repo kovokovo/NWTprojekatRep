e01d4e15ad85ba9972c8579e774dbc1b9cfecc4b
acesko1 <acesko1@etf.unsa.ba>
2020-03-26 11:59:11 +0100

    Update StavkaProdajeService.java

--- korisnickiServis/src/main/java/in/acesko/springbootcrud/springbootcrudapi/service/StavkaProdajeService.java
+++ korisnickiServis/src/main/java/in/acesko/springbootcrud/springbootcrudapi/service/StavkaProdajeService.java
@@ -1,5 +1,11 @@
 package in.acesko.springbootcrud.springbootcrudapi.service;
+import java.util.List;
+import in.acesko.springbootcrud.springbootcrudapi.model.StavkaProdaje;
 
 public interface StavkaProdajeService {
-
+	
+	List<StavkaProdaje> get();
+	StavkaProdaje get(int id);
+	void save(StavkaProdaje proizvod);
+	void delete(int id);
 }
